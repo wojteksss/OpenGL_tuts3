64a568b11526b765df6ee966a24e5ad946f3fa27
Wojtek <wojtekpilwinski@gmail.com>
2018-07-24 19:11:22 +0200

    Initial Commit

---
+++ OpenGL_tuts3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_tuts3
+//
+//  Created by Wojtek Pilwinski on 24/07/2018.
+//  Copyright © 2018 wojteksss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
